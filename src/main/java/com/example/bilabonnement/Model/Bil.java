4db4f1eb4595f5fba2b5627abc2996daf31b288d
Bush0058 <buja0001@stud.kea.dk>
2024-12-04 14:46:07 +0100

    -  tjek om konstruktør er med i bil.java

--- src/main/java/com/example/bilabonnement/Model/Bil.java
+++ src/main/java/com/example/bilabonnement/Model/Bil.java
@@ -1,14 +1,105 @@
 package com.example.bilabonnement.Model;
 
 public class Bil {
-    private String Vognnummer;
-    private String Stelnummer;
-    private int Model;
-    private double Staalpris;
-    private double RegAfgift;
-    private double CO2Udledning;
-    private String GearType;
-    private String Billede;
-    private Tilstand Tilstand;
-}
+    private String vognnummer;
+    private String stelnummer;
+    private int model;
+    private double staalpris;
+    private double regAfgift;
+    private double co2Udledning;
+    private String gearType;
+    private String billede;
+    private Tilstand tilstand;
 
+    public Bil(String vognnummer, String stelnummer, int model, double staalpris, double regAfgift, double co2Udledning, String gearType, String billede, Tilstand tilstand) {
+        this.vognnummer = vognnummer;
+        this.stelnummer = stelnummer;
+        this.model = model;
+        this.staalpris = staalpris;
+        this.regAfgift = regAfgift;
+        this.co2Udledning = co2Udledning;
+        this.gearType = gearType;
+        this.billede = billede;
+        this.tilstand = tilstand;
+    }
+
+    // Getters and Setters
+    public String getVognnummer() {
+        return vognnummer;
+    }
+
+    public void setVognnummer(String vognnummer) {
+        this.vognnummer = vognnummer;
+    }
+
+    public String getStelnummer() {
+        return stelnummer;
+    }
+
+    public void setStelnummer(String stelnummer) {
+        this.stelnummer = stelnummer;
+    }
+
+    public int getModel() {
+        return model;
+    }
+
+    public void setModel(int model) {
+        this.model = model;
+    }
+
+    public double getStaalpris() {
+        return staalpris;
+    }
+
+    public void setStaalpris(double staalpris) {
+        this.staalpris = staalpris;
+    }
+
+    public double getRegAfgift() {
+        return regAfgift;
+    }
+
+    public void setRegAfgift(double regAfgift) {
+        this.regAfgift = regAfgift;
+    }
+
+    public double getCo2Udledning() {
+        return co2Udledning;
+    }
+
+    public void setCo2Udledning(double co2Udledning) {
+        this.co2Udledning = co2Udledning;
+    }
+
+    public String getGearType() {
+        return gearType;
+    }
+
+    public void setGearType(String gearType) {
+        this.gearType = gearType;
+    }
+
+    public String getBillede() {
+        return billede;
+    }
+
+    public void setBillede(String billede) {
+        this.billede = billede;
+    }
+
+    public Tilstand getTilstand() {
+        return tilstand;
+    }
+
+    public void setTilstand(Tilstand tilstand) {
+        this.tilstand = tilstand;
+
+
+    }
+    @Override
+    public String toString(){
+        return "Vognnummer"+this.vognnummer+"Stelnummer"+this.stelnummer+"Model"+this.model+"Staalpris"+this.staalpris+"RegAfgift"+ this.regAfgift+"Co2Udledning"+this.co2Udledning+"GearType"+this.gearType+"Billede"+this.billede+"Tilstand"+this.tilstand;
+    }
+
+}
