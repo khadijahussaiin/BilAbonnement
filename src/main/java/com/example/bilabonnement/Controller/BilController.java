b768410d6075b8ec24024a34e4f192b0456a242b
Khadija <khhu0001@stud.kea.dk>
2024-12-05 09:19:52 +0100

    Ændringer i Model

--- src/main/java/com/example/bilabonnement/Controller/BilController.java
+++ src/main/java/com/example/bilabonnement/Controller/BilController.java
@@ -1,4 +1,12 @@
 package com.example.bilabonnement.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BilController {
+    @GetMapping("/Bil")
+    public String ShowBil() {
+        return "home/BilAdministration";
+    }
 }
--- src/main/java/com/example/bilabonnement/Model/Kunde.java
+++ src/main/java/com/example/bilabonnement/Model/Kunde.java
@@ -4,4 +4,30 @@ public class Kunde {
     private String Navn;
     private String Email;
     private String Telefon;
+
+    public Kunde() {
+    }
+    public Kunde(String navn, String email, String telefon) {
+        this.Navn = navn;
+        this.Email = email;
+        this.Telefon = telefon;
+    }
+    public String getNavn() {
+        return Navn;
+    }
+    public void setNavn(String Navn) {
+        this.Navn = Navn;
+    }
+    public String getEmail() {
+        return Email;
+    }
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+    public String getTelefon() {
+        return Telefon;
+    }
+    public void setTelefon(String Telefon) {
+        this.Telefon = Telefon;
+    }
 }
