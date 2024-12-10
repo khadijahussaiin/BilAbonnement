0672100114c9c4da4d558d9052a8b19d249b65f7
Khadija <khhu0001@stud.kea.dk>
2024-12-10 12:20:48 +0100

    Frontend færdig af Bil Administration

--- src/main/java/com/example/bilabonnement/Service/UdlejeværdiService.java
+++ src/main/java/com/example/bilabonnement/Service/UdlejeværdiService.java
@@ -4,7 +4,6 @@ import com.example.bilabonnement.Repository.UdlejeværdiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Map;
 
 @Service
