e001026140421a00bc107087953ac2cb10ea641d
Khadija <khhu0001@stud.kea.dk>
2024-12-17 13:43:02 +0100

    Azure ændret til 21 for deployment kan fungerer

--- src/test/java/com/example/bilabonnement/BilAbonnementApplicationTests.java
+++ src/test/java/com/example/bilabonnement/BilAbonnementApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.bilabonnement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class BilAbonnementApplicationTests {
 
     @Test
